06442e1896b4700f9d646f83f121c0c794e3022c
Bryce Mecum <petridish@gmail.com>
2021-01-25 15:12:55 -0900

    Initial Commit

---
+++ Shared/MonitorApp.swift
@@ -0,0 +1,17 @@
+//
+//  MonitorApp.swift
+//  Shared
+//
+//  Created by Bryce Mecum on 1/25/21.
+//
+
+import SwiftUI
+
+@main
+struct MonitorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
