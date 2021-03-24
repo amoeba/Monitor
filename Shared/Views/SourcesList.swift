8158e1ad1db262c15f0032a1e7e8ba102de7a02d
Bryce Mecum <petridish@gmail.com>
2021-03-23 18:53:59 -0800

    Break SourcesList for Apple bug report FB9045695

--- Shared/Views/SourcesList.swift
+++ Shared/Views/SourcesList.swift
@@ -17,7 +17,8 @@ struct SourcesList: View {
                 }
                 .onDelete(perform: delete)
             }
-            .navigationTitle("Sources")
+            .navigationTitle(nil)
+            .navigationBarHidden(true)
         }
     }
 }
